6d26514b13102c9e516088b655ca15a5c80c4df9
Hanspreet Singh <98222754+hshanjra@users.noreply.github.com>
2024-05-08 22:17:51 +0530

    update roles guard: added verification logic

--- src/api/auth/guards/roles.guard.ts
+++ src/api/auth/guards/roles.guard.ts
@@ -1,4 +1,9 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Role } from '../enums';
 
@@ -19,10 +24,18 @@ export class RolesGuard implements CanActivate {
     const request = context.switchToHttp().getRequest();
     const user = request.user;
 
-    if (!user) {
+    if (!user || !user.roles || user.roles.length === 0) {
       return false; // No user, deny access
     }
 
+    //if user email is not verified
+    if (!user.isEmailVerified)
+      throw new UnauthorizedException('Email is not verified.');
+
+    // if merchant is not verified.
+    if (user.roles.includes(Role.SELLER) && !user.merchant.isVerified) {
+      throw new UnauthorizedException('You are not verified as a seller.');
+    }
     return requiredRoles.every((role) => user.roles.includes(role));
   }
 }
